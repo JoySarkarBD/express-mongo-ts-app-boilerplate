07ce0875621059439c9336bad33027014a659a2b
JoySarkarBD <developer.joysarkar@gmail.com>
2024-09-19 21:20:04 +0600

    chore: instead of bcrypt now it will use bcryptjs

--- src/utils/bcrypt/hash-info.ts
+++ src/utils/bcrypt/hash-info.ts
@@ -1,14 +1,14 @@
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 /**
- * Hashes the given data using bcryptjs.
+ * Hashes the given data using bcrypt.
  *
  * @param {string} data - The data to be hashed.
  * @returns {Promise<string>} - A promise that resolves to the hashed data.
  */
 const HashInfo = async (data: string) => {
-  // Hash the data using bcryptjs with a salt rounds value of 10
-  return await bcryptjs.hash(data, 10);
+  // Hash the data using bcrypt with a salt rounds value of 10
+  return await bcrypt.hash(data, 10);
 };
 
 export default HashInfo;
